5bed3bb4fb65c51b48348034a627ed03242ae8c2
Aditya Soni <adityasoni5859@gmail.com>
2018-12-11 14:52:32 +0530

    invoking method in ios using platform channels.

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -7,6 +7,18 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    FlutterViewController* controller = (FlutterViewController*) self.window.rootViewController;
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"soni.aditya.com/info"
+                                     binaryMessenger:controller];
+    [channel setMethodCallHandler:^(FlutterMethodCall* call,FlutterResult result){
+        NSString *from = call.arguments[@"from"];
+        if([@"getMessage" isEqualToString:call.method]){
+            NSString *message = @"IOS says Hi !";
+            NSString *returnMessage = [message stringByAppendingString:from];
+            result(returnMessage);
+        }
+    }];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
