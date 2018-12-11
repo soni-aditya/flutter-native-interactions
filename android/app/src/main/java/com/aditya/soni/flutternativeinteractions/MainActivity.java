e64ca05df7d76f3791127685180521e577b45f74
Aditya Soni <adityasoni5859@gmail.com>
2018-12-11 13:10:23 +0530

    Invoking platform channel in android.

--- android/app/src/main/java/com/aditya/soni/flutternativeinteractions/MainActivity.java
+++ android/app/src/main/java/com/aditya/soni/flutternativeinteractions/MainActivity.java
@@ -1,13 +1,30 @@
 package com.aditya.soni.flutternativeinteractions;
 
 import android.os.Bundle;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    private static final String CHANNEL = "soni.aditya.com/info";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                        if (methodCall.method.equals("getMessage")) {
+                            String message = "Android Says Hi.";
+                            result.success(message);
+                        }
+                    }
+                }
+        );
+    }
 }
